4c6ae576daa783ec8383b65c5f10f475b40279b8
juliareynolds11 <61146850+juliareynolds11@users.noreply.github.com>
2020-04-06 18:20:02 +0200

    Update getTWSpreads.R
    Added docstring

--- Codes/Clean Data/getTWSpreads.R
+++ Codes/Clean Data/getTWSpreads.R
@@ -1,4 +1,27 @@
 function(endSpread, spread, mid, time, intervals, q) {
+  #' Calculate time-weighted bid-ask spreads.
+  #'
+  #' @description This function calculates time-weighted bid-ask spreads (both absolute spreads and relative to midquote) aggregated
+  #' across a pre-defined interval length  (e.g., 60-second intervals). 
+  #'
+  #' Define n as the number of observed order book updates during interval t. Define T the total number of intervals.
+  #'
+  #' @param endSpread Vector of length 3 containing the information about the limit order book at the 
+  #' end of interval t-1; information is in the following order: c(bid-ask spread (askprice-bidprice), 
+  #' midquote (0.5*(askprice+bidprice)),timestamp (in # of seconds past midnight))
+  #' (needed in order to determine the state of the limit order book at the beginning of interval t).
+  #' @param spread Vector of length n of spreads (askprice-bidprice) during interval t.
+  #' @param mid Vector of length n of midquotes (0.5*(askprice+bidprice)) during interval t.
+  #' @param time Vector of length n of timestamps (in # of seconds past midnight) of order book updates during interval t.
+  #' @param intervals Vector of length T of timestamps of cutoffs between intervals (i.e., first timestamp of each
+  #' interval).
+  #' @param q Scalar. Index of the timestamp corresponding to the beginning of interval t within the above vector 
+  #' "intervals".
+  #' 
+  #' @usage getTWSpreads(endSpread, spread, mid, time, intervals, q) 
+  #' 
+  #' @return Vector of length 2 containing: (1) absoute time-weighted bid-ask spread ("ABS.SPR.TW"), and (2) 
+  #' relative (to the midquote) time-weighted bid-ask spread ("REL.SPR.TW") during interval t
   
   quotesttemp <- as.data.frame(rbind(endSpread, cbind(spread, mid, time)))
   quotesttemp$time[1] <- intervals[q]
